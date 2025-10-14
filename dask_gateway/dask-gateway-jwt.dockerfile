c3341ac12ee63880fc5a50d7e6eeb383fdc422f2
Gerald Walter Irsiegler <gerald.irsiegler@eodc.eu>
2025-10-14 14:36:27 +0200

    Rewrite: jwt auth

--- dask_gateway/dask-gateway-jwt.dockerfile
+++ dask_gateway/dask-gateway-jwt.dockerfile
@@ -3,5 +3,6 @@ ARG DASK_EXT_DIR=/home/dask/dask-server-extensions
 RUN pip install --no-cache-dir pyjwt && \
     mkdir $DASK_EXT_DIR
 COPY dask_jwt_authenticator.py $DASK_EXT_DIR/dask_jwt_authenticator.py
+COPY jwks.json $DASK_EXT_DIR/jwks.json
 ENV PYTHONPATH="$DASK_EXT_DIR"
 
--- dask_gateway/dask_jwt_authenticator.py
+++ dask_gateway/dask_jwt_authenticator.py
@@ -1,10 +1,9 @@
-import functools
-import contextvars
-import os
-
-import asyncio
-import aiohttp
+import base64
+import uuid
 import jwt
+import aiohttp
+import json
+import os
 
 from aiohttp import web
 from traitlets import Dict, Unicode, default
@@ -12,106 +11,137 @@ from traitlets.config import LoggingConfigurable
 
 from dask_gateway_server.auth import Authenticator, unauthorized, User
 
+dask_roles = {
+    "stack-dask-high": {
+        "worker_cores": 12,
+        "worker_memory": 24,
+    },
+    "stack-dask-medium": {
+        "worker_cores": 4,
+        "worker_memory": 8,
+    },
+    "stack-dask-low": {
+        "worker_cores": 1,
+        "worker_memory": 2,
+    },
+}
 
-class JwtAuthenticator(Authenticator, LoggingConfigurable):
 
-    jwks_url = Unicode(
+class JWTAuthenticator(Authenticator, LoggingConfigurable):
+    jwks_urls = Dict(
         help="""
-        The JWKS URL to get the public keys to verify JWT.
-
-        By default this is determined from the ``JWKS_URL``
-        environment variable.
+        A mapping of validator names to their JWKS URLs to get the public keys to verify JWT.
+        The validator name is expected to be present in the "auth_extra" claim of the JWT.
         """,
         config=True,
     )
 
-    @default("jwks_url")
-    def _default_jwks_url(self):
-        out = os.environ.get("JWKS_URL")
-        if not out:
-            raise ValueError("JWKS_URL must be set")
-        return out
+    @default("jwks_urls")
+    def _default_jwks_urls(self):
+        config_string = os.getenv("JWKS_URL", "")
 
-    #jwks_url = "https://identity.data.destination-earth.eu/auth/realms/dedl/protocol/openid-connect/certs"  # json
+        config: dict = json.loads(base64.decode(config_string))
+        jwks_urls = {}
 
-    # Known Dask roles, order is important, from high to low
-    profiles = Dict()
-    @default("profiles")
-    def _default_profiles(self):
-        return {
-            "stack-dask-high": {
-                "worker_cores": 12,
-                "worker_memory": 24,
-            },
-            "stack-dask-medium": {
-                "worker_cores": 4,
-                "worker_memory": 8,
-            },
-            "stack-dask-low":{
-                "worker_cores": 1,
-                "worker_memory": 2,
-            }
-        }
+        for auth_extra, jwks_url in config.items():
+            jwks_urls[auth_extra] = jwks_url
+
+        return jwks_urls
 
     async def setup(self, app):
-        self.dask_roles = set(self.profiles.copy().keys())
         self.session = aiohttp.ClientSession()
-        self._jwks_client = jwt.PyJWKClient(self.jwks_url)
-
-    # Maybe premature optimization... get_signing_key_from_jwt is blocking call,
-    # so wrap it around a thread, maybe.
-    async def _wrapped_get_signing_key_from_jwt(self, token):
-        # no asyncio.to_thread in 3.8 :(
-        loop = asyncio.get_event_loop()
-        ctx = contextvars.copy_context()
-        func = self._jwks_client.get_signing_key_from_jwt
-        func_call = functools.partial(ctx.run, func, token)
-        signing_key = await loop.run_in_executor(None, func_call)
-        return signing_key
-
-    def validate(self, signing_key, token):
-        return jwt.decode(token, signing_key.key,
-            audience="account", algorithms=["RS256"])
 
     async def cleanup(self):
         if hasattr(self, "session"):
             await self.session.close()
 
     async def authenticate(self, request):
-        auth_headers = request.headers.get("Authorization")
-        if not auth_headers:
+        self.log.info("Authenticating request...")
+        auth_header: str = request.headers.get("Authorization")
+        if not auth_header:
+            self.log.info("No Authorization header.")
             raise web.HTTPUnauthorized(reason="No JWT in Headers.")
 
-        # not happy with this, but :shrug:
         try:
-            idx = auth_headers.index("Bearer ")
+            assert auth_header.startswith("Bearer ")
         except ValueError:
+            self.log.info("No 'Bearer' in Authorization header.")
             raise web.HTTPUnauthorized(reason="No 'Bearer' in Authorization header.")
-        token = auth_headers[idx + 7 :]  # len("Bearer ") == 7
 
-        signing_key = await self._wrapped_get_signing_key_from_jwt(token)
+        token = auth_header.split(" ")[-1]
+
         try:
-            data = self.validate(signing_key, token)
-        except jwt.exceptions.ExpiredSignatureError as e:
-            self.log.debug("JWT: Expired Key")
-            raise unauthorized("expired jwt")
-
-        if data and \
-            ("realm_access" in data and "preferred_username" in data) and \
-                "roles" in data["realm_access"]:
-            user_name = data["preferred_username"]
-            roles = set(data["realm_access"]["roles"])
-            user_dask_role = sorted(roles.intersection(self.dask_roles))
-            if len(user_dask_role) == 0:
-                self.log.error(f"Service access for user {user_name} declined.")
-                raise unauthorized("jwt")
-            else:
-                self.log.info(f"User {user_name} access granted with role {user_dask_role[0]}.")
-                return User(
-                    user_name,
-                    groups=list(roles),
-                    admin=False,
-                )
-        else:
-            self.log.error("JWT: No data in token validation.")
+            split_token = token.split("/")
+        except Exception:
+            self.log.error("Error splitting token.")
             raise unauthorized("jwt")
+
+        if len(split_token) != 2:
+            auth_extra = "eodc"
+        else:
+            auth_extra = split_token[0]
+            token = split_token[1]
+
+        if auth_extra not in self.jwks_urls:
+            self.log.error(f"Unknown auth_extra '{auth_extra}'")
+            raise unauthorized("jwt")
+        else:
+            self.jwks_url = self.jwks_urls[auth_extra]
+            self._jwks_client = jwt.PyJWKClient(self.jwks_url)
+
+        self.log.info(f"Authenticating with auth_extra '{auth_extra}'")
+        if auth_extra == "eodc":
+            return await eodc_validate_token(token, self._jwks_client)
+        elif auth_extra == "tuw":
+            return await tuw_validate_token(token, self._jwks_client)
+
+        raise unauthorized("jwt")
+
+
+async def pre_validation(token, jwks_client: jwt.PyJWKClient):
+    signing_key = jwks_client.get_signing_key_from_jwt(token)
+    try:
+        data = jwt.decode(token, signing_key.key, algorithms=["RS256"])
+    except jwt.exceptions.ExpiredSignatureError as e:
+        raise unauthorized("Couldn't validate jwt")
+
+    return data
+
+
+async def eodc_validate_token(token, jwks_client: jwt.PyJWKClient):
+    data = await pre_validation(jwks_client, token)
+
+    if data and ("realm_access" in data) and ("roles" in data["realm_access"]):
+        if "preferred_username" not in data:
+            user_name = str(uuid.uuid4())
+        else:
+            user_name = data["preferred_username"]
+        roles = set(data["realm_access"]["roles"])
+        user_dask_role = sorted(roles.intersection(dask_roles))
+        if len(user_dask_role) == 0:
+            raise unauthorized("jwt")
+        else:
+            return User(
+                user_name,
+                groups=[user_dask_role[0]],
+                admin=False,
+            )
+
+    raise unauthorized("Not authorized for Dask Gateway")
+
+
+async def tuw_validate_token(token, jwks_client: jwt.PyJWKClient):
+    data = await pre_validation(token, jwks_client)
+
+    if ("eodc_access" in data) and (data["eodc_access"]):
+        if "preferred_username" not in data:
+            user_name = str(uuid.uuid4())
+        else:
+            user_name = data["preferred_username"]
+        return User(
+            user_name,
+            groups=dask_roles["stack-dask-high"],
+            admin=False,
+        )
+
+    raise unauthorized("Not authorized for Dask Gateway")
